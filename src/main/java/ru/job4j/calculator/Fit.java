136277dc71a6a217177dd6e5619733ccaef4aede
Hotsko Vladimir <malletgamer@mail.ru>
2023-11-08 22:39:37 +0300

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        double rsl = height - 100 * 1.15;
+        return rsl;
+    }
+
+    public static double womanWeight(short height) {
+        double rsl = height - 110 * 1.15;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        short manHeight = 180;
+        short womanHeight = 170;
+
+        double man = Fit.manWeight(manHeight);
+        double girl = Fit.womanWeight(womanHeight);
+
+        System.out.println("Man 180 is " + man);
+        System.out.println("Girl 170 is " + girl);
+    }
+}
